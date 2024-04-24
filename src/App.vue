bed227f25298f7a4715f084d40177759ea1fbdf5
kvvasu <lukaszkwas96@gmail.com>
2024-04-24 22:34:43 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -63,98 +63,11 @@
 </template>
 
 <script>
-const kwasu = {
-  height: 184,
-  weight: 93,
-  held_items: [
-    {
-      item: {
-        name: "metal-powder",
-        url: "https://pokeapi.co/api/v2/item/234/",
-      },
-    },
-    {
-      item: {
-        name: "quick-powder",
-        url: "https://pokeapi.co/api/v2/item/251/",
-      },
-    },
-  ],
-  cries: {
-    latest:
-      "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/45.ogg",
-  },
-  id: 0,
-  name: "Kwasu",
-  sprites: {
-    front_default:
-      "https://raw.githubusercontent.com/kvvasuu/pokemon-search/master/src/assets/kvvasu.png",
-  },
-  stats: [
-    {
-      base_stat: 99,
-      effort: 1,
-      stat: {
-        name: "hp",
-        url: "https://pokeapi.co/api/v2/stat/1/",
-      },
-    },
-    {
-      base_stat: 99,
-      effort: 0,
-      stat: {
-        name: "attack",
-        url: "https://pokeapi.co/api/v2/stat/2/",
-      },
-    },
-    {
-      base_stat: 99,
-      effort: 0,
-      stat: {
-        name: "defense",
-        url: "https://pokeapi.co/api/v2/stat/3/",
-      },
-    },
-    {
-      base_stat: 99,
-      effort: 0,
-      stat: {
-        name: "special-attack",
-        url: "https://pokeapi.co/api/v2/stat/4/",
-      },
-    },
-    {
-      base_stat: 99,
-      effort: 0,
-      stat: {
-        name: "special-defense",
-        url: "https://pokeapi.co/api/v2/stat/5/",
-      },
-    },
-    {
-      base_stat: 99,
-      effort: 0,
-      stat: {
-        name: "speed",
-        url: "https://pokeapi.co/api/v2/stat/6/",
-      },
-    },
-  ],
-  types: [
-    {
-      slot: 1,
-      type: {
-        name: "normal",
-        url: "https://pokeapi.co/api/v2/type/1/",
-      },
-    },
-  ],
-};
-
 import StatsTable from "./components/StatsTable.vue";
 import PokemonDisplay from "./components/PokemonDisplay.vue";
 import HeldItems from "./components/HeldItems.vue";
 import HelpModal from "./components/HelpModal.vue";
+import { kwasu } from "./components/EasterEgg";
 
 export default {
   name: "App",
